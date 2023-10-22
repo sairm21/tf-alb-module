f1330ccc3838f072c17bf69907a4a2777d14b894
sairam <sairm21@gmail.com>
2023-10-22 16:51:18 +0530

    ssl traffic redirection

--- main.tf
+++ main.tf
@@ -63,3 +63,19 @@ resource "aws_lb_listener" "main" {
     }
   }
 }
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
